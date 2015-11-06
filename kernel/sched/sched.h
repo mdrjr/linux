8134cc901594d88387ac4d7c25a18a69e4b88aed
Arseniy Krasnov <a.krasnov at>
2015-11-06 12:02:38 +0000

    hperf_hmp: scheduler initialization routines.
    setup, which initializes some HMP scheduler variables: big and little cluster
    masks. They are read from kernel config(if set), else default values are used.
    
    Signed-off-by: Tarek Dakhran <t.dakhran <at> samsung.com>
    Signed-off-by: Sergey Dyasly <s.dyasly <at> samsung.com>
    Signed-off-by: Dmitriy Safonov <d.safonov <at> partner.samsung.com>
    Signed-off-by: Arseniy Krasnov <a.krasnov <at> samsung.com>
    Signed-off-by: Ilya Maximets <i.maximets <at> samsung.com>
    Signed-off-by: memeka <mihailescu2m@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -711,6 +711,11 @@ struct rq {
 	 */
 	unsigned long nr_uninterruptible;
 
+#ifdef CONFIG_HPERF_HMP
+	/* shows the amount of accumulated unfairness by tasks of this rq */
+	long druntime_sum;
+	unsigned int nr_hmp_tasks;
+#endif
 	struct task_struct *curr, *idle, *stop;
 	unsigned long next_balance;
 	struct mm_struct *prev_mm;
@@ -1129,6 +1134,16 @@ static inline void dirty_sched_domain_sysctl(int cpu)
 }
 static inline void unregister_sched_domain_sysctl(void)
 {
+}
+#endif
+
+#ifdef CONFIG_HPERF_HMP
+extern struct cpumask *cpu_fastest_mask;
+extern struct cpumask *cpu_slowest_mask;
+
+static inline bool cpu_is_fastest(int cpu)
+{
+	return cpumask_test_cpu(cpu, cpu_fastest_mask);
 }
 #endif
 
