e5d10080766001f43a05a70299e09c40aae74153
Arseniy Krasnov <a.krasnov at>
2015-11-06 12:02:40 +0000

    hperf_hmp: introduce druntime metric.
    This patch adds special per-task metric to look for candidate for
    migration between HMP domains(clusters). 'druntime' grows up when task runs on
    A7 cluster, and goes down on A15 cluster. Also druntime is scaled according load
    on little cluster in order to align its value with big cluster's total druntime.
    For migration from big/little to little/big cluster task with lowest/highest
    'druntime' chosen. 'druntime' is used to execute each task on each cluster
    approximately same amount of time. 'druntime' is calculated each call of default
    'update_curr' function.
    
    Signed-off-by: Tarek Dakhran <t.dakhran <at> samsung.com>
    Signed-off-by: Sergey Dyasly <s.dyasly <at> samsung.com>
    Signed-off-by: Dmitriy Safonov <d.safonov <at> partner.samsung.com>
    Signed-off-by: Arseniy Krasnov <a.krasnov <at> samsung.com>
    Signed-off-by: Ilya Maximets <i.maximets <at> samsung.com>
    Signed-off-by: memeka <mihailescu2m@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -381,6 +381,9 @@ struct sched_entity {
 	struct list_head		group_node;
 	unsigned int			on_rq;
 
+#ifdef CONFIG_HPERF_HMP
+	long			druntime;
+#endif
 	u64				exec_start;
 	u64				sum_exec_runtime;
 	u64				vruntime;
