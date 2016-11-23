855488fec7ff34a8e5b5ddee6ad778b8c778c243
Joy Cho <joy.cho@hardkernel.com>
2016-11-23 14:45:25 +0900

    ODROID-C1: Add a new overclocking cpu frequency
    - Add 1824MHz
    - Adjust sys_pll_table of 1728MHz
    
    Change-Id: Ia464ba7cfdcaa209580e1501b91fba7211cd8453

--- arch/arm/mach-meson8b/include/mach/cpufreq_table.h
+++ arch/arm/mach-meson8b/include/mach/cpufreq_table.h
@@ -22,7 +22,8 @@ static struct cpufreq_frequency_table meson_freq_table[]=
     {15 , 1536000  },
     {16 , 1632000  },
     {17 , 1728000  },
-    {18	, CPUFREQ_TABLE_END},
+    {18 , 1824000  },
+    {19	, CPUFREQ_TABLE_END},
 };
 
 static struct cpufreq_frequency_table meson_freq_table_fix_syspll[] = {
@@ -46,5 +47,6 @@ static struct cpufreq_frequency_table meson_freq_table_fix_syspll[] = {
     {15 , 1536000  },
     {16 , 1632000  },
     {17 , 1728000  },
-    {18	, CPUFREQ_TABLE_END},
+    {18 , 1824000  },
+    {19	, CPUFREQ_TABLE_END},
 };
