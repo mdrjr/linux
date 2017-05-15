ac54f9f006246261c67dd3443b0feb12ceba9f83
Mauro (mdrjr) Ribeiro <mauro.ribeiro@hardkernel.com>
2017-05-15 18:16:30 -0300

    drm/exynos: backport FBIOGET_DMABUF from 3.10 kernel
    backport by @owersun originally for the 4.9 kernel
    
    Signed-off-by: memeka <mihailescu2m@gmail.com>

--- drivers/gpu/drm/exynos/exynos_drm_fbdev.c
+++ drivers/gpu/drm/exynos/exynos_drm_fbdev.c
@@ -63,6 +63,24 @@ static int exynos_drm_fb_mmap(struct fb_info *info,
 	return 0;
 }
 
+static struct dma_buf *exynos_fb_get_dma_buf(struct fb_info *info)
+{
+	struct dma_buf *buf = NULL;
+	struct drm_fb_helper *helper = info->par;
+	struct drm_device *dev = helper->dev;
+	struct exynos_drm_fbdev *exynos_fbd = to_exynos_fbdev(helper);
+	struct exynos_drm_gem *exynos_gem = exynos_fbd->exynos_gem;
+
+	if( dev->driver->gem_prime_export ) {
+		buf = dev->driver->gem_prime_export( dev, &exynos_gem->base, O_RDWR);
+		if(buf) {
+			drm_gem_object_reference(&exynos_gem->base);
+		}
+	}
+
+	return buf;
+}
+
 static struct fb_ops exynos_drm_fb_ops = {
 	.owner		= THIS_MODULE,
 	DRM_FB_HELPER_DEFAULT_OPS,
@@ -70,6 +88,7 @@ static struct fb_ops exynos_drm_fb_ops = {
 	.fb_fillrect	= drm_fb_helper_cfb_fillrect,
 	.fb_copyarea	= drm_fb_helper_cfb_copyarea,
 	.fb_imageblit	= drm_fb_helper_cfb_imageblit,
+	.fb_dmabuf_export = exynos_fb_get_dma_buf,
 };
 
 static int exynos_drm_fbdev_update(struct drm_fb_helper *helper,
