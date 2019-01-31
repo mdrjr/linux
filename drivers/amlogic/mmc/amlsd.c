27e09f0e57b270a0843086ff3ff3b23ebcc7cc97
Kevin Kim <ckkim@hardkernel.com>
2019-01-31 16:40:41 +0900

    ODROID-N2: mmc: Initate MMC power state on reboot
    Change-Id: Ifade734854d94c1c98b34c55a431835b80c5f7e4

--- drivers/amlogic/mmc/amlsd.c
+++ drivers/amlogic/mmc/amlsd.c
@@ -384,6 +384,7 @@ int of_amlsd_init(struct amlsd_platform *pdata)
 		}
 	}
 #endif
+#if !defined(CONFIG_ARCH_MESON64_ODROID_COMMON)
 	if (pdata->gpio_power) {
 		if (pdata->power_level &&
 		    !aml_card_type_non_sdio(pdata)) {
@@ -396,6 +397,7 @@ int of_amlsd_init(struct amlsd_platform *pdata)
 			CHECK_RET(ret);
 		}
 	}
+#endif
 
 	/* if(pdata->port == MESON_SDIO_PORT_A) */
 	/* wifi_setup_dt(); */
