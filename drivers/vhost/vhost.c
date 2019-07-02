5540e98728c8cc2f397e23f31a0bfc2e2224be35
OtherCrashOverride <OtherCrashOverride@users.noreply.github.com>
2019-07-01 21:17:19 -0500

    Enable KVM. Add SPI flash definition (not working).

--- drivers/vhost/vhost.c
+++ drivers/vhost/vhost.c
@@ -740,7 +740,7 @@ static int vhost_copy_to_user(struct vhost_virtqueue *vq, void *to,
 		if (ret < 0)
 			goto out;
 		iov_iter_init(&t, WRITE, vq->iotlb_iov, ret, size);
-		ret = copy_to_iter(from, size, &t);
+		ret = copy_to_iter((void*)from, size, &t);
 		if (ret == size)
 			ret = 0;
 	}
