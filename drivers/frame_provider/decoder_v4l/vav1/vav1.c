e3edb7f1353630f1768243bc9040324353f82c22
shihong.zheng <shihong.zheng@amlogic.com>
2024-07-23 19:53:53 +0800

    av1: CB1 av1 decode mosaic. [1/1]
    PD#SWPL-178764
    
    Problem:
    av1 mosaic on sc2 platform.
    
    Solution:
    add hevc stream register control config
    revert bug code.
    
    Verify:
    s905x4
    
    Change-Id: Ie65128ab85247aee430089e943bebef694c2e3c6
    Signed-off-by: shihong.zheng <shihong.zheng@amlogic.com>

--- drivers/frame_provider/decoder_v4l/vav1/vav1.c
+++ drivers/frame_provider/decoder_v4l/vav1/vav1.c
@@ -9688,6 +9688,8 @@ int vav1_set_isreset(struct vdec_s *vdec, int isreset)
 
 static void vav1_prot_init(struct AV1HW_s *hw, u32 mask)
 {
+	u32 data32;
+
 	av1_print(hw, AOM_DEBUG_HW_MORE, "%s %d\n", __func__, __LINE__);
 
 	aom_config_work_space_hw(hw, mask);
@@ -9709,7 +9711,7 @@ static void vav1_prot_init(struct AV1HW_s *hw, u32 mask)
 
 	if (debug & AV1_DEBUG_BUFMGR_MORE)
 		pr_info("%s\n", __func__);
-#if 0
+
 	data32 = READ_VREG(HEVC_STREAM_CONTROL);
 	data32 = data32 |
 		(1 << 0)/*stream_fetch_enable*/
@@ -9725,7 +9727,7 @@ static void vav1_prot_init(struct AV1HW_s *hw, u32 mask)
 				 ;
 	    WRITE_VREG(HEVC_STREAM_FIFO_CTL, data32);
 	}
-#endif
+
 	WRITE_VREG(HEVC_SHIFT_STARTCODE, 0x000000001);
 	WRITE_VREG(HEVC_SHIFT_EMULATECODE, 0x00000300);
 
