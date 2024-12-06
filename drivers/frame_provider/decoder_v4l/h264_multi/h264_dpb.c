f63fdd8a6ebaf05b3970955f3cb350cc3742f5dc
xing.xu <xing.xu@amlogic.com>
2024-12-06 16:47:37 +0800

    h264: CB1 H264 playback Stuck [1/1]
    PD#SWPL-190850
    
    Problem:
    H264 playback Stuck
    
    Solution:
    Modify loop conditions
    
    Verify:
    s7d
    
    Change-Id: Id3df92dca34ae27530493c86dfdd07a81f53bb4c
    Signed-off-by: xing.xu <xing.xu@amlogic.com>

--- drivers/frame_provider/decoder_v4l/h264_multi/h264_dpb.c
+++ drivers/frame_provider/decoder_v4l/h264_multi/h264_dpb.c
@@ -2024,7 +2024,7 @@ int output_frames(struct h264_dpb_stru *p_H264_Dpb, unsigned char flush_flag)
 {
 	int poc, pos;
 	struct DecodedPictureBuffer *p_Dpb = &p_H264_Dpb->mDPB;
-	int i;
+	int i,j;
 	int none_displayed_num = 0;
 	unsigned char fast_output_flag = 0;
 	int inner_dpb_size = 0;
@@ -2083,8 +2083,8 @@ int output_frames(struct h264_dpb_stru *p_H264_Dpb, unsigned char flush_flag)
 					(((long)p_Dpb->fs[i]->poc -
 						(long)p_Dpb->last_output_poc)
 					== 1)) {
-					for (i = 0; i < p_Dpb->used_size; i++) {
-						if (p_Dpb->fs[i]->data_flag & ERROR_FLAG)
+					for (j = 0; j < p_Dpb->used_size; j++) {
+						if (p_Dpb->fs[j]->data_flag & ERROR_FLAG)
 							break;
 
 						fast_output_flag = 1;
